6e3faf2702b1310fa9c3bade9ddf6435ca71af64
Zach Lobato <zacharyisaiahlobato@hotmail.com>
2024-04-14 21:07:28 -0600

    feature - title page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,28 @@
 <template>
+  <header class="h-screen flex flex-col justify-around">
+    <div></div>
+    <div class="flex flex-col justify-center">
+      <h1 class="text-5xl text-center text-pistachio-200 mb-4">Zach Lobato</h1>
+      <h2 class="text-xl text-center text-pistachio-200 mb-20">Husband, Father, Software Engineer</h2>
+      <p class="text-lg text-cadet-gray-200 text-base text-center">I make products customers love.</p>
+    </div>
+    <ul v-if="false">
+      <li><a href="#Products">Products</a></li>
+    </ul>
+    <ul class="flex flex-row space-x-8 justify-center">
+      <li><a target="_blank" href="https://bsky.app/profile/zachlobato.bsky.social" class="dark:text-prussian-blue-200 text-prussian-blue-800 hover:text-mint-green-800 dark:hover:text-mint-green-200"><IconBlueSky></IconBlueSky></a></li>
+      <li><a target="_blank" href="https://github.com/ZachLobato" class="dark:text-prussian-blue-200 text-prussian-blue-800 hover:text-mint-green-800 dark:hover:text-mint-green-200"><IconGithub></IconGithub></a></li>
+      <li><a target="_blank" href="https://www.instagram.com/zachlobato" class="dark:text-prussian-blue-200 text-prussian-blue-800 hover:text-mint-green-800 dark:hover:text-mint-green-200"><IconInstagram></IconInstagram></a></li>
+      <li><a target="_blank" href="https://www.linkedin.com/in/zachlobato" class="dark:text-prussian-blue-200 text-prussian-blue-800 hover:text-mint-green-800 dark:hover:text-mint-green-200"><IconLinkedIn></IconLinkedIn></a></li>
+    </ul>
+  </header>
   <main>
     <div class="flex">
-      <div class="m-auto">
-        <h2 class="text-2xl text-prussian-blue-700 dark:text-prussian-blue-300 text-center mb-4">Portfolio</h2>
+      <div class="m-auto min-h-screen">
+        <h2 id="Products" class="flex flex-col px-4 justify-center grid-row text-3xl text-pistachio-800 dark:text-pistachio-200 mb-6 h-16 bg-cadet-gray-200 dark:bg-cadet-gray-800 mx-0">
+          Products
+        </h2>
+        <h3 class="flex flex-col px-4 justify-center grid-row text-lg text-pistachio-800 dark:text-pistachio-200 mb-8 mx-0">Latest Updated</h3>
         <div class="flex justify-center gap-4 flex-wrap">
           <div v-if="data.isLoading">Loading...</div>
           <MaterialCard v-else v-for="(card, index) in data.info" :key="index"
@@ -25,6 +45,11 @@
 <script setup>
 import MaterialCard from '../components/MaterialCard.vue'
 import { onMounted, reactive } from 'vue';
+import IconBlueSky from '../components/icons/IconBlueSky.vue'
+import IconGithub from '../components/icons/IconGithub.vue'
+import IconInstagram from '../components/icons/IconInstagram.vue'
+import IconLinkedIn from '../components/icons/IconLinkedIn.vue'
+
 
 // Define reactive state to store API data and loading state
 const data = reactive({
